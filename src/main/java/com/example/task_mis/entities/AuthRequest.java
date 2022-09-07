c90e4ff4de8f38e86c4a4bcdbba036f19e8f1791
mehreennajm <mehreen.najm@gmail.com>
2022-09-07 15:00:08 +0200

    Implemented JWT token

---
+++ src/main/java/com/example/task_mis/entities/AuthRequest.java
@@ -0,0 +1,23 @@
+package com.example.task_mis.entities;
+
+public class AuthRequest {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUserName(String userName) {
+        this.username = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.password = userPassword;
+    }
+}
