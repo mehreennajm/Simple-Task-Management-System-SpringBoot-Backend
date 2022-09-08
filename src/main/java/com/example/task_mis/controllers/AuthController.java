085fb23e3a217ad9837da8f81b81bb1a005ab02c
mehreennajm <mehreen.najm@gmail.com>
2022-09-08 15:23:27 +0200

    Updated AuthController

--- src/main/java/com/example/task_mis/controllers/AuthController.java
+++ src/main/java/com/example/task_mis/controllers/AuthController.java
@@ -6,25 +6,19 @@ import com.example.task_mis.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api")
 public class AuthController {
     @Autowired
     private JwtUtil jwtUtil;
     @Autowired
     private AuthenticationManager authenticationManager;
 
-    @GetMapping("/")
-    public String welcome() {
-        return "Welcome to javatechie !!";
-    }
 
 
-    @PostMapping("/authenticate")
+    @PostMapping({"/authenticate"})
     public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try {
             authenticationManager.authenticate(
